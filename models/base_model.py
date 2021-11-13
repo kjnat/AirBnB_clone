028c1ae9b037dc15d80b5fa200ece06516891686
kjnat <46638938+kjnat@users.noreply.github.com>
2021-11-13 20:39:02 +0000

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,1 +1,45 @@
+#!/usr/bin/python3
+"""Defines the BaseModel class."""
+import models
+from uuid import uuid4
+from datetime import datetime
 
+
+class BaseModel:
+    """Represents the custom BaseModel """
+
+    def __init__(self, *args, **kwargs):
+        """Initialization method for instance"""
+        tform = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, tform)
+                else:
+                    self.__dict__[k] = v
+        else:
+            models.storage.new(self)
+
+    def save(self):
+        """Update public instance attribute
+           updated_at with the current datetime.
+        """
+        self.updated_at = datetime.today()
+        models.storage.save()
+
+    def __str__(self):
+        """Return the print/str representation of the BaseModel instance."""
+        clname = self.__class__.__name__
+        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
+
+    def to_dict(self):
+        """Method returns a dictionary containing all keys/values
+        of __dict__ instance"""
+        rdict = self.__dict__.copy()
+        rdict["created_at"] = self.created_at.isoformat()
+        rdict["updated_at"] = self.updated_at.isoformat()
+        rdict["__class__"] = self.__class__.__name__
+        return rdict
