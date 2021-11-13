7a796e7a2aafd8c9c76c33947ee14771f300ed04
kjnat <46638938+kjnat@users.noreply.github.com>
2021-11-13 20:39:29 +0000

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,1 +1,9 @@
+#!/user/bin/python3
+""" Defines the City class. """
+from models.base_model import BaseModel
 
+
+class City(BaseModel):
+    """ Represent a city """
+    state_id = ""
+    name = ""
